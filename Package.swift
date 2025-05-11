0bd8b4dceac74d03c5766e7d3a73c7700db35f68
MakeMeMonad <173313719+MakeMeMonad@users.noreply.github.com>
2025-05-11 16:17:03 -0400

    Added Swift Testing target

--- Package.swift
+++ Package.swift
@@ -10,5 +10,9 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "AgencyKitXCMCP"),
-    ]
+.testTarget(
+    name: "AgencyKitXCMCPTests",
+    dependencies: ["AgencyKitXCMCP"]
+),
+]
 )
